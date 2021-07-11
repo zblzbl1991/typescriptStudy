8b887c62d046a950d8df5c8d44215a25d1286b34
zblzbl1991 <357760876@qq.com>
2021-07-11 17:43:33 +0800

    添加ts 继承,vue

---
+++ my-vue-project/src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png">
+  <HelloWorld msg="Welcome to Your Vue.js App"/>
+</template>
+
+<script lang="ts">
+
+// import HelloWorld from './components/HelloWorld.vue'
+import {defineComponent} from "vue";
+
+export default defineComponent({
+  name: 'App',
+  components: {},
+  setup(){
+    console.log('zbl')
+  },
+  data() {
+    return {
+      name: String
+    }
+  }
+})
+// export default {
+//
+// }
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/继承.ts
@@ -0,0 +1,44 @@
+﻿class Person2{
+    private _name:string
+    private _sex:'男'|'女'
+
+    constructor(name: string, sex: "男" | "女") {
+        this._name = name;
+        this._sex = sex;
+    }
+
+    say(){
+        console.log('默认')
+    }
+
+
+    get name(): string {
+        return this._name;
+    }
+
+    set name(value: string) {
+        this._name = value;
+    }
+
+    get sex(): "男" | "女" {
+        return this._sex;
+    }
+
+    set sex(value: "男" | "女") {
+        this._sex = value;
+    }
+}
+class Man extends Person2{
+
+
+    say() {
+       console.log(this.sex)
+    }
+}
+class Woman extends Person2{
+
+}
+let man = new Man('zbl','男');
+let woman = new Woman('lzh','女');
+man.say()
+woman.say()
